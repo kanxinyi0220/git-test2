d1fcfb7bfa1aa8556c5373bbc0e0553b1e205b0a
kxy <2537002034@qq.com>
2022-05-12 12:02:46 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/git.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author kxy
+ * @date 2022/5/12 11:58
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
